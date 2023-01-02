234080f5ad20b7b67a24d61ad29311c0e2982bc7
Miguel Prada <mapra99@gmail.com>
2023-01-01 20:48:45 -0500

    parse all numeric inputs to floats

--- lib/materials/concrete.rb
+++ lib/materials/concrete.rb
@@ -3,8 +3,8 @@ module Materials
     attr_reader :elastic_module, :design_compression_strength
 
     def initialize(elastic_module:, design_compression_strength:)
-      @elastic_module = elastic_module
-      @design_compression_strength = design_compression_strength
+      @elastic_module = elastic_module.to_f
+      @design_compression_strength = design_compression_strength.to_f
     end
   end
 end
--- lib/materials/soil.rb
+++ lib/materials/soil.rb
@@ -3,7 +3,7 @@ module Materials
     attr_reader :bearing_capacity
 
     def initialize(bearing_capacity:)
-      @bearing_capacity = bearing_capacity
+      @bearing_capacity = bearing_capacity.to_f
     end
   end
 end
