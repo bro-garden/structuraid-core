282870cbe7c70245afc1fcfdb8088e90722c6203
immprada <im.mprada@gmail.com>
2023-01-03 11:28:31 -0500

    upgrade by discussion

--- lib/elements/footing.rb
+++ lib/elements/footing.rb
@@ -1,25 +1,12 @@
 module Elements
   class Footing
-    attr_accessor :step, :length_x, :length_y, :height, :material
+    attr_reader :step, :length_x, :length_y, :height, :material
 
-    def initialize(length_x:, length_y:, height:, concrete:, step: 100)
+    def initialize(length_x:, length_y:, height:, material:)
       @length_x = length_x.to_f
       @length_y = length_y.to_f
       @height = height.to_f
-      @step = step.to_f
-      @material = concrete
-    end
-
-    def incrase_height_by_step
-      @height = height + step
-    end
-
-    def incrase_length_x_by_step
-      @length_x = length_x.to_f + step
-    end
-
-    def incrase_length_y_by_step
-      @length_y = length_y.to_f + step
+      @material = material
     end
 
     def horizontal_area
--- spec/elements/footing_spec.rb
+++ spec/elements/footing_spec.rb
@@ -3,52 +3,15 @@ require 'elements/footing'
 require 'materials/concrete'
 
 RSpec.describe Elements::Footing do
-  subject(:footing) { described_class.new(dimension1: 100, dimension2: 100, height: 100, concrete:) }
+  subject(:footing) { described_class.new(length_x: 100, length_y: 100, height: 100, material: concrete) }
 
-  let(:concrete) { Materials::Concrete.new_reinfoced_concrete(elastic_module: 20_000, design_compression_strength: 20) }
-
-  describe '#change_concrete_to' do
-    let(:concrete2) do
-      Materials::Concrete.new_reinfoced_concrete(elastic_module: 20_000, design_compression_strength: 24)
-    end
-
-    it 'changes concrete' do
-      footing.change_concrete_to(concrete2)
-
-      expect(footing.concrete).to eq(concrete2)
-    end
+  let(:concrete) do
+    Materials::Concrete.new(elastic_module: 20_000, design_compression_strength: 20, specific_weight: 24)
   end
 
-  describe '#incrase_height_by_step' do
-    it 'increases height by step' do
-      initial_height = footing.height
-      footing.incrase_height_by_step
-
-      expect(footing.height).to eq(initial_height + footing.step)
-    end
-  end
-
-  describe '#incrase_dimension1_by_step' do
-    it 'increases dimension1 by step' do
-      initial_dimension1 = footing.dimension1
-      footing.incrase_dimension1_by_step
-
-      expect(footing.dimension1).to eq(initial_dimension1 + footing.step)
-    end
-  end
-
-  describe '#incrase_dimension2_by_step' do
-    it 'increases dimension2 by step' do
-      initial_dimension2 = footing.dimension2
-      footing.incrase_dimension2_by_step
-
-      expect(footing.dimension2).to eq(initial_dimension2 + footing.step)
-    end
-  end
-
-  describe '#area' do
-    it 'returns area' do
-      expect(footing.area).to eq(footing.dimension1 * footing.dimension2)
+  describe '#horizontal_area' do
+    it 'returns horizontal area' do
+      expect(footing.horizontal_area).to eq(footing.length_x * footing.length_y)
     end
   end
 end
