e48531ddf51440c00960b2a94b7660136d46b174
miguel <26731448+ImMPrada@users.noreply.github.com>
2023-01-03 11:01:51 -0500

    Update lib/elements/steel_reinforcement/rebar_hook.rb
    Co-authored-by: Miguel Prada <mapra99@gmail.com>

--- lib/elements/steel_reinforcement/rebar_hook.rb
+++ lib/elements/steel_reinforcement/rebar_hook.rb
@@ -13,16 +13,6 @@ module Elements
 
       private
 
-      def calculate_length
-        case angle
-        when 90
-          12 * diameter
-        when 180
-          4 * diameter
-        else
-          6 * diameter
-        end
-      end
     end
   end
 end
