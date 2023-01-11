9843f1e6c470334b9c80cce38e145fc330064b79
immprada <im.mprada@gmail.com>
2023-01-11 07:45:11 -0500

    create specs files for Loads::PointLoad and Loads::UniformLoad

---
+++ spec/loads/point_load_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'loads/point_load'
+
+RSpec.describe Loads::PointLoad do
+  xit 'does something'
+end
---
+++ spec/loads/uniform_load_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'loads/uniform_load'
+
+RSpec.describe Loads::UniformLoad do
+  xit 'does something'
+end
