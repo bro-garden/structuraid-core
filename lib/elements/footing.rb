3b5cf5a5ef3676f521d5bbb730acd93e6c6c31fb
immprada <im.mprada@gmail.com>
2023-02-13 17:05:57 -0500

    upgrade Elements::Footing

--- lib/elements/footing.rb
+++ lib/elements/footing.rb
@@ -1,16 +1,27 @@
 module Elements
   class Footing
-    attr_reader :length_x, :length_y, :height, :material
+    attr_reader :length_1, :length_2, :height, :material, :cover_lateral, :cover_top, :cover_bottom
 
-    def initialize(length_x:, length_y:, height:, material:)
-      @length_x = length_x.to_f
-      @length_y = length_y.to_f
+    def initialize(
+      length_1:,
+      length_2:,
+      height:,
+      material:,
+      cover_lateral:,
+      cover_top:,
+      cover_bottom:
+    )
+      @length_1 = length_1.to_f
+      @length_2 = length_2.to_f
       @height = height.to_f
       @material = material
+      @cover_lateral = cover_lateral.to_f
+      @cover_top = cover_top.to_f
+      @cover_bottom = cover_bottom.to_f
     end
 
     def horizontal_area
-      length_x * length_y
+      length_1 * length_2
     end
   end
 end
