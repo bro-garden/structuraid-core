2929ffdcd64700ef57e73ef4c7c86c823138f167
immprada <im.mprada@gmail.com>
2023-02-16 13:40:44 -0500

    upgrade Elements::Footing class, since now it has new parameters, meking it avaible to know its own reinforcment

--- lib/elements/footing.rb
+++ lib/elements/footing.rb
@@ -9,7 +9,9 @@ module Elements
       material:,
       cover_lateral:,
       cover_top:,
-      cover_bottom:
+      cover_bottom:,
+      longitudinal_top_reinforcement:,
+      longitudinal_bottom_reinforcement:
     )
       @length_1 = length_1.to_f
       @length_2 = length_2.to_f
@@ -18,10 +20,20 @@ module Elements
       @cover_lateral = cover_lateral.to_f
       @cover_top = cover_top.to_f
       @cover_bottom = cover_bottom.to_f
+      @reinforcement_length_1 = nil
+      @reinforcement_length_1 = nil
+      @longitudinal_top_reinforcement = longitudinal_top_reinforcement
+      @longitudinal_bottom_reinforcement = longitudinal_bottom_reinforcement
     end
 
     def horizontal_area
       length_1 * length_2
+    end
+
+    private
+
+    def aspect_ratio
+      @length_1 / @length_2
     end
   end
 end
