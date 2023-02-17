84229ce220b2fbe4aafaf4f0fbe5a9717f737837
immprada <im.mprada@gmail.com>
2023-02-17 11:37:02 -0500

    update Elements::Reinforcement::StraightLongitudinal specs

--- lib/elements/reinforcement/straight_longitudinal.rb
+++ lib/elements/reinforcement/straight_longitudinal.rb
@@ -4,16 +4,22 @@ require 'elements/reinforcement/layout'
 module Elements
   module Reinforcement
     class StraightLongitudinal < Base
-      def initialize(z_base:)
-        @z_base = z_base.to_f
+      def initialize(z_base:, direction: 1)
+        @z_base = z_base
+        @direction = direction
         @layouts = []
+      end
+
+      def modify_z_base(z_base:)
+        @z_base = z_base.to_f
+        update_z_base_of_layouts unless @layouts.empty?
       end
 
       def add_layout(start_location:, end_location:)
         start_location.value_z = @z_base
         end_location.value_z = @z_base
         id = @layouts.empty? ? 1 : @layouts.last.id
-        @layouts << Elements::Reinforcement::Layout.new(start_location, end_location, id)
+        @layouts << Elements::Reinforcement::Layout.new(start_location:, end_location:, id:)
       end
 
       def add(number_of_rebars:, rebar:, length:)
@@ -27,6 +33,13 @@ module Elements
       end
 
       private
+
+      def update_z_base_of_layouts
+        @layouts.each do |layout|
+          layout.start_location.value_z = @z_base
+          layout.end_location.value_z = @z_base
+        end
+      end
 
       def available_layout
         @layouts.find(&:available?)
--- spec/elements/reinforcement/straight_longitudinal_spec.rb
+++ spec/elements/reinforcement/straight_longitudinal_spec.rb
@@ -1,6 +1,63 @@
 require 'spec_helper'
 require 'elements/reinforcement/straight_longitudinal'
+require 'engineering/locations/relative'
 
 RSpec.describe Elements::Reinforcement::StraightLongitudinal do
-  xit 'does something'
+  subject(:reinforcement) { described_class.new(z_base: 450, direction: -1) }
+
+  let(:start_location) { Engineering::Locations::Relative.new(value_x: -950, value_y: -450, value_z: 0) }
+  let(:end_location) { Engineering::Locations::Relative.new(value_x: 950, value_y: 450, value_z: 0) }
+
+  describe '#add_layout' do
+    before do
+      reinforcement.add_layout(start_location:, end_location:)
+    end
+
+    it 'adds layout' do
+      expect(reinforcement.instance_variable_get(:@layouts).size).to eq(1)
+    end
+
+    it 'sets the start_location of the layout' do
+      expect(reinforcement.instance_variable_get(:@layouts).first.start_location).to eq(start_location)
+    end
+
+    it 'sets the end_location of the layout' do
+      expect(reinforcement.instance_variable_get(:@layouts).first.end_location).to eq(end_location)
+    end
+  end
+
+  describe '#modify_z_base' do
+    describe 'when there is no layouts' do
+      before do
+        reinforcement.modify_z_base(z_base: 500)
+      end
+
+      it 'updates z_base property' do
+        expect(reinforcement.instance_variable_get(:@z_base)).to eq(500)
+      end
+
+      it 'keeps layouts empty' do
+        expect(reinforcement.instance_variable_get(:@layouts)).to be_empty
+      end
+    end
+
+    describe 'when there are layouts' do
+      before do
+        reinforcement.add_layout(start_location:, end_location:)
+        reinforcement.modify_z_base(z_base: 500)
+      end
+
+      it 'updates z_base property' do
+        expect(reinforcement.instance_variable_get(:@z_base)).to eq(500)
+      end
+
+      it 'updates value_z at start_location of layouts' do
+        expect(reinforcement.instance_variable_get(:@layouts).first.start_location.value_z).to eq(500)
+      end
+
+      it 'updates value_z at end_location of layouts' do
+        expect(reinforcement.instance_variable_get(:@layouts).first.end_location.value_z).to eq(500)
+      end
+    end
+  end
 end
