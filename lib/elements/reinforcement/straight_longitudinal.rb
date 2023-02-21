3f0c54bd7620b8b23f4e409a196690a42032b337
immprada <im.mprada@gmail.com>
2023-02-21 15:41:30 -0500

    upgrafe Elements::Reinforcement::StraightLongitudinal by discussion

--- lib/elements/reinforcement/straight_longitudinal.rb
+++ lib/elements/reinforcement/straight_longitudinal.rb
@@ -7,22 +7,16 @@ module Elements
     class StraightLongitudinal < Base
       attr_accessor :layers
 
-      def initialize(z_base:, distribution_direction:, direction: 1)
-        @z_base = z_base
-        @direction = direction
+      def initialize(distribution_direction:, above_middle: false)
+        @above_middle = above_middle
         @layers = []
         @distribution_direction = distribution_direction
       end
 
-      def modify_z_base(z_base:)
-        @z_base = z_base.to_f
-        update_z_base_of_layers unless @layers.empty?
-      end
-
       def add_layer(start_location:, end_location:, number_of_rebars:, rebar:)
-        start_location.value_3 = @z_base
-        end_location.value_3 = @z_base
         id = @layers.empty? ? 1 : @layers.last.id
+        start_location = modify_value_3_of_location(start_location, 0.5 * rebar.diameter)
+        end_location = modify_value_3_of_location(end_location, 0.5 * rebar.diameter)
         @layers << Elements::Reinforcement::StraightLongitudinalLayer.new(
           start_location:,
           end_location:,
@@ -35,9 +29,14 @@ module Elements
         @layers.last
       end
 
-      def change(id_of_layer_to_change:, number_of_rebars:, rebar:)
+      def change(id_of_layer_to_change:, number_of_rebars:, rebar:, start_location: nil, end_location: nil)
         straight_longitudinal_layer = find(id_of_layer_to_change)
-        straight_longitudinal_layer.modify(number_of_rebars:, rebar:)
+        straight_longitudinal_layer.modify(
+          number_of_rebars:,
+          rebar:,
+          start_location:,
+          end_location:
+        )
 
         straight_longitudinal_layer
       end
@@ -74,6 +73,10 @@ module Elements
         @layers.find do |existing_straight_longitudinal_layer|
           existing_straight_longitudinal_layer.id == id_of_layer_to_change
         end
+      end
+
+      def modify_value_3_of_location(location, offset)
+        location.value_3 = above_middle ? location.value_3 - offset : location.value_3 + offset
       end
     end
   end
