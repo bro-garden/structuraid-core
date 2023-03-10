545434b097a9785ec42b7693a657d0c08d5fcb5b
immprada <im.mprada@gmail.com>
2023-03-10 11:09:32 -0500

    add axes transformation to Engineering::Locations::Relative

--- lib/engineering/locations/relative.rb
+++ lib/engineering/locations/relative.rb
@@ -1,22 +1,61 @@
 require 'engineering/locations/base'
+require 'engineering/array'
+require 'byebug'
 
 module Engineering
   module Locations
     class Relative < Base
-      attr_accessor :value_1, :value_2, :value_3
+      attr_reader :value_1, :value_2, :value_3, :angle
 
-      def self.absolute_location_relative_to(location_to_relativize:, reference_location:)
+      def self.from_location_to_location(location_to_relativize:, reference_location:)
+        array_location = location.to_a
+
         new(
-          value_1: location_to_relativize.value_x - reference_location.value_x,
-          value_2: location_to_relativize.value_y - reference_location.value_y,
-          value_3: location_to_relativize.value_z - reference_location.value_z
+          value_i: array_location[0][0],
+          value_j: array_location[1][0],
+          value_k: array_location[2][0]
         )
       end
 
-      def initialize(value_1:, value_2:, value_3:)
+      def initialize(value_1:, value_2:, value_3:, origin:)
         @value_1 = value_1.to_f
         @value_2 = value_2.to_f
         @value_3 = value_3.to_f
+        @origin = origin
+        @angle = 0.0
+      end
+
+      def align_axis_1_with(vector:)
+        @angle = Math.atan2(vector.value_j, vector.value_i)
+
+        transformed = transformer_array_global_to_local * to_a
+        @value_1 = transformed[0][0]
+        @value_2 = transformed[1][0]
+        @value_3 = transformed[2][0]
+
+        self
+      end
+
+      def to_a
+        Engineering::Array.new(
+          [value_1],
+          [value_2],
+          [value_3]
+        )
+      end
+
+      def to_vector
+        Engineering::Vector.based_on_location(location: self)
+      end
+
+      private
+
+      def transformer_array_global_to_local
+        Engineering::Array.new(
+          [Math.cos(angle), Math.sin(angle), 0],
+          [-Math.sin(angle), Math.cos(angle), 0],
+          [0, 0, 1]
+        )
       end
     end
   end
---
+++ spec/engineering/locations/relative_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+require 'engineering/locations/relative'
+require 'engineering/locations/absolute'
+require 'engineering/array'
+require 'engineering/vector'
+
+RSpec.describe Engineering::Locations::Relative do
+  subject(:relative) { described_class.new(value_1:, value_2:, value_3:, origin:) }
+
+  let(:value_1) { 3.0 }
+  let(:value_2) { 4.0 }
+  let(:value_3) { 6.0 }
+  let(:origin) { Engineering::Locations::Absolute.new(value_x: 10.0, value_y: 15.0, value_z: 3.0) }
+
+  describe '#to_a' do
+    it 'returns an Engineering::Array' do
+      expect(relative.to_a).to be_an_instance_of(Engineering::Array)
+    end
+
+    it 'returns right module' do
+      expect(relative.to_a).to match_array([[value_1], [value_2], [value_3]])
+    end
+  end
+
+  describe '#to_vector' do
+    it 'returns an Engineering::Vector' do
+      expect(relative.to_vector).to be_an_instance_of(Engineering::Vector)
+    end
+
+    it 'returns a vector with same components values as the relative location components' do
+      expect(relative.to_vector.to_a).to match_array(relative.to_a)
+    end
+  end
+
+  describe '#align_axis_1_with' do
+    let(:another_relative) do
+      described_class.new(
+        value_1: -value_1,
+        value_2: -value_2,
+        value_3:,
+        origin:
+      )
+    end
+    let(:vector) { Engineering::Vector.based_on_location(location: relative) }
+
+    it 'updates angle' do
+      relative.align_axis_1_with(vector:)
+      expect(relative.angle).not_to be(0.0)
+    end
+
+    it "updates location's coordinates" do
+      another_relative.align_axis_1_with(vector:)
+      expect(another_relative.to_a).to match_array([[-5.0], [0.0], [6.0]])
+    end
+  end
+end
