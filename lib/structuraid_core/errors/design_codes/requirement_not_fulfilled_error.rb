3fedc93a5edd74e43a5dc7287f98fcd4f81bd4d0
Miguel Prada <mapra99@gmail.com>
2023-03-15 06:32:55 -0500

    feat(design codes): add requirement for footing min height

---
+++ lib/structuraid_core/errors/design_codes/requirement_not_fulfilled_error.rb
@@ -0,0 +1,15 @@
+module StructuraidCore
+  module DesignCodes
+    class RequirementNotFulfilledError < StandardError
+      attr_reader :requirement, :message, :code_reference
+
+      def initialize(requirement, message, code_reference)
+        @requirement = requirement
+        @message = message
+        @code_reference = code_reference
+
+        super(message)
+      end
+    end
+  end
+end
