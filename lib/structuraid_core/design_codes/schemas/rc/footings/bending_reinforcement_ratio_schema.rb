b314eb1db2355ad51807c0eb6ee4f5f77a53334f
immprada <im.mprada@gmail.com>
2023-03-30 16:46:12 -0500

    feat(rc-footings): add bending reinforcement ratio schema for reinforced concrete footings.

---
+++ lib/structuraid_core/design_codes/schemas/rc/footings/bending_reinforcement_ratio_schema.rb
@@ -0,0 +1,17 @@
+module StructuraidCore
+  module DesignCodes
+    module Schemas
+      module RC
+        module Footings
+          class BendingReinforcementRatioSchema
+            include DesignCodes::Utils::SchemaDefinition
+
+            required_params []
+
+            optional_params []
+          end
+        end
+      end
+    end
+  end
+end
