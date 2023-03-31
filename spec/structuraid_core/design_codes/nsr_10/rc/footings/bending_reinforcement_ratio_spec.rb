ba94cf7a6e28fcd47980b69f2f21fae49155b797
immprada <im.mprada@gmail.com>
2023-03-31 10:12:21 -0500

    feat(bending_reinforcement_ratio_spec.rb): add spec for bending reinforcement ratio calculation function.

---
+++ spec/structuraid_core/design_codes/nsr_10/rc/footings/bending_reinforcement_ratio_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+# rubocop:disable RSpec/FilePath
+RSpec.describe StructuraidCore::DesignCodes::NSR10::RC::Footings::BendingReinforcementRatio do
+  describe '.call' do
+    subject(:result) do
+      described_class.call(
+        design_compression_strength:,
+        design_steel_yield_strength:,
+        width:,
+        effective_height:,
+        flexural_moment:,
+        capacity_reduction_factor:
+      )
+    end
+
+    let(:design_compression_strength) { 28_000_000 } # N/(m**2)
+    let(:design_steel_yield_strength) { 420_000_000 } # N/(m**2)
+    let(:width) { 2.5 } # m
+    let(:effective_height) { 0.45 } # m
+    let(:capacity_reduction_factor) { 0.90 }
+  end
+end
+# rubocop:enable RSpec/FilePath
