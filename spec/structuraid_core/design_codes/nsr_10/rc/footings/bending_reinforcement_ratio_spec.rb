c6fe5ba5c939c8f0a97cde4ea81df77fe4865519
immprada <im.mprada@gmail.com>
2023-03-31 10:13:03 -0500

    test(bending_reinforcement_ratio_spec.rb): add test for concrete with 28MPa strength

--- spec/structuraid_core/design_codes/nsr_10/rc/footings/bending_reinforcement_ratio_spec.rb
+++ spec/structuraid_core/design_codes/nsr_10/rc/footings/bending_reinforcement_ratio_spec.rb
@@ -14,11 +14,22 @@ RSpec.describe StructuraidCore::DesignCodes::NSR10::RC::Footings::BendingReinfor
       )
     end
 
-    let(:design_compression_strength) { 28_000_000 } # N/(m**2)
     let(:design_steel_yield_strength) { 420_000_000 } # N/(m**2)
     let(:width) { 2.5 } # m
     let(:effective_height) { 0.45 } # m
     let(:capacity_reduction_factor) { 0.90 }
+    
+    describe 'when concrete is 28MPa' do
+      let(:design_compression_strength) { 28_000_000 } # N/(m**2)
+
+      describe 'when flexural moment is small' do
+        let(:flexural_moment) { 50 } # N*m
+  
+        it 'returns minimal ratio' do
+          expect(result).to eq(StructuraidCore::DesignCodes::NSR10::RC::Footings::BendingReinforcementRatio::MINIMUM_RATIO)
+        end
+      end
+    end
   end
 end
 # rubocop:enable RSpec/FilePath
