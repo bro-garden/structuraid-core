5cd0a03b71e1fe2639593d62882904449fbef4c5
immprada <im.mprada@gmail.com>
2023-04-03 10:59:38 -0500

    feat(reduction_factor_spec.rb): add test for tension_controlled strength controlling behaviour in NSR10 RC reduction factor calculation

--- spec/structuraid_core/design_codes/nsr_10/rc/reduction_factor_spec.rb
+++ spec/structuraid_core/design_codes/nsr_10/rc/reduction_factor_spec.rb
@@ -16,6 +16,20 @@ RSpec.describe StructuraidCore::DesignCodes::NSR10::RC::ReductionFactor do
         expect { result }.to raise_error(StructuraidCore::DesignCodes::UnrecognizedValueError)
       end
     end
+
+    describe 'when strength controlling is tension_controlled' do
+      subject(:result) do
+        described_class.call(
+          strength_controlling_behaviour:
+        )
+      end
+
+      let(:strength_controlling_behaviour) { :tension_controlled }
+
+      it 'returns the right reduction factor' do
+        expect(result).to eq(0.90)
+      end
+    end
   end
 end
 # rubocop:enable RSpec/FilePath
