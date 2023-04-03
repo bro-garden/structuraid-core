acecd77b3606adbfdb5a18a8ef7bb517c9f5f94d
immprada <im.mprada@gmail.com>
2023-04-03 11:14:43 -0500

    feat(reduction_factor_spec.rb): add support for compression_controlled strength controlling behaviour and steel coils reinforcement type

--- spec/structuraid_core/design_codes/nsr_10/rc/reduction_factor_spec.rb
+++ spec/structuraid_core/design_codes/nsr_10/rc/reduction_factor_spec.rb
@@ -30,6 +30,39 @@ RSpec.describe StructuraidCore::DesignCodes::NSR10::RC::ReductionFactor do
         expect(result).to eq(0.90)
       end
     end
+
+    describe 'when strength controlling is compression_controlled' do
+      subject(:result) do
+        described_class.call(
+          params
+        )
+      end
+
+      describe 'when reinforcement is provided by steel coils' do
+        let(:params) do
+          {
+            strength_controlling_behaviour: :compression_controlled,
+            is_coil_rebar: true
+          }
+        end
+
+        it 'returns the right reduction factor' do
+          expect(result).to eq(0.75)
+        end
+      end
+
+      describe 'when reinforcement is provided by other steel rebar' do
+        let(:params) do
+          {
+            strength_controlling_behaviour: :compression_controlled
+          }
+        end
+
+        it 'returns the right reduction factor' do
+          expect(result).to eq(0.65)
+        end
+      end
+    end
   end
 end
 # rubocop:enable RSpec/FilePath
