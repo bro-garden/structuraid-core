01d3b6313fc34b456c00c9351c178942b14b6a07
miguel <26731448+ImMPrada@users.noreply.github.com>
2023-04-03 15:59:39 -0500

    Feat/shear capacity (#51)

---
+++ lib/structuraid_core/design_codes/schemas/rc/footings/one_way_shear_capacity_schema.rb
@@ -0,0 +1,22 @@
+module StructuraidCore
+  module DesignCodes
+    module Schemas
+      module RC
+        module Footings
+          class OneWayShearCapacitySchema
+            include DesignCodes::Utils::SchemaDefinition
+
+            required_params %i[
+              design_compression_strength
+              width
+              effective_height
+              light_concrete_modification_factor
+            ]
+
+            optional_params []
+          end
+        end
+      end
+    end
+  end
+end
