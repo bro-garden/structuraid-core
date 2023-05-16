5d1df99da8af422972b11de76a62022cddd73f56
Miguel Prada <mapra99@gmail.com>
2023-05-15 22:46:53 -0500

    linter

--- spec/factories/elements/reinforcement/straight_longitudinal.rb
+++ spec/factories/elements/reinforcement/straight_longitudinal.rb
@@ -1,7 +1,8 @@
 require 'factory_bot'
 
 FactoryBot.define do
-  factory :straight_longitudinal_reinforcement, class: 'StructuraidCore::Elements::Reinforcement::StraightLongitudinal' do
+  factory :straight_longitudinal_reinforcement,
+          class: 'StructuraidCore::Elements::Reinforcement::StraightLongitudinal' do
     distribution_direction { %i[length_1 length_2].sample }
     above_middle { [true, false].sample }
 
