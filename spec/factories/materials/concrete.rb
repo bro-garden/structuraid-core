a57049675c15b06d37b6342aa9d550591e1660bf
Miguel Prada <mapra99@gmail.com>
2023-05-16 09:00:05 -0500

    Update spec/factories/materials/concrete.rb
    Co-authored-by: miguel <26731448+ImMPrada@users.noreply.github.com>

--- spec/factories/materials/concrete.rb
+++ spec/factories/materials/concrete.rb
@@ -2,8 +2,8 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :concrete, class: 'StructuraidCore::Materials::Concrete' do
-    elastic_module { 24_870.06 } # MPa
-    design_compression_strength { 28 } # MPa
+    elastic_module { 24_870.06 } # N/mm2
+    design_compression_strength { 28 } # N/mm2
     specific_weight { 0.000024 } # N/mm3
 
     skip_create
