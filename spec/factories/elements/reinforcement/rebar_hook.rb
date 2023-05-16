3b526fd633de19dcf09e4d504ef66df4f92ebe95
Miguel Prada <mapra99@gmail.com>
2023-05-16 08:59:36 -0500

    Update spec/factories/elements/reinforcement/rebar_hook.rb
    Co-authored-by: miguel <26731448+ImMPrada@users.noreply.github.com>

--- spec/factories/elements/reinforcement/rebar_hook.rb
+++ spec/factories/elements/reinforcement/rebar_hook.rb
@@ -3,7 +3,7 @@ require 'factory_bot'
 FactoryBot.define do
   factory :rebar_hook, class: 'StructuraidCore::Elements::Reinforcement::RebarHook' do
     material { build(:steel) }
-    number { (2..8).to_a.sample }
+    number { rand(2..8) }
 
     skip_create
     initialize_with { new(**attributes) }
