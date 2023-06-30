a7d515b29f27a8a49dd0c1423f98310861d396c5
immprada <im.mprada@gmail.com>
2023-06-30 10:12:18 -0500

    add #add_reinforcement case

--- lib/structuraid_core/designers/footings/steps/centric_isolated/add_or_check_reinforcement_ratio.rb
+++ lib/structuraid_core/designers/footings/steps/centric_isolated/add_or_check_reinforcement_ratio.rb
@@ -12,8 +12,27 @@ module StructuraidCore
             # @param footing [StructuraidCore::Elements::Footing] The footing to be designed
             # @param analysis_direction [Symbol] The direction for which the analysis has to be run. Should be either :length_1 or :length2
             # @param analysis_results [Hash] The analysis results
+            # @param design_code [StructuraidCore::DesignCodes] The design code to be used
+            # @param steel [StructuraidCore::Materials::Steel] The rebar's material
+            # @param support_type [Symbol or String] The support type: :over_soil or :over_piles
+            # @param analysis_direction [Symbol] The direction for which the analysis has to be run. Should be either :length_1 or :length2
             def call
+              return add_reinforcement if footing_has_reinforcement?
 
+              check_reinforcement_ratio
+            end
+
+            private
+
+            def footing_has_reinforcement?
+              context.footing.reinforcement_ratio(
+                direction: context.analysis_direction, above_middle: false
+              ).nil?
+            end
+
+            def add_reinforcement
+              Steps::CentricIsolated::SetReinforcementLayersCoordinatesToFooting.call!(context)
+              Steps::CentricIsolated::SetInitialLongitudinalReinforcement.call!(context)
             end
           end
         end
---
+++ spec/structuraid_core/designers/footings/steps/centric_isolated/add_or_check_reinforcement_ratio_spec.rb
@@ -0,0 +1,108 @@
+require 'spec_helper'
+require 'byebug'
+
+RSpec.describe(
+  StructuraidCore::Designers::Footings::Steps::CentricIsolated::AddOrCheckReinforcementRatio
+) do
+  describe '.call' do
+    subject(:result_with_mocked_required_reinforcement_ratio) { described_class.call(params) }
+
+    let(:params) do
+      {
+        footing:,
+        analysis_direction: :length_1,
+        design_code: StructuraidCore::DesignCodes::Nsr10,
+        steel: build(:steel),
+        support_type: :over_soil,
+        analysis_results:
+      }
+    end
+
+    let(:coordinates_system) { build(:coordinates_system) }
+
+    before do
+      footing.add_coordinates_system(coordinates_system)
+      footing.add_vertices_location
+    end
+
+    describe "when footing doesn't have reinforcement" do
+      let(:footing) do
+        build(
+          :footing,
+          length_1: 1500,
+          length_2: 1500,
+          height: 500,
+          material: build(:concrete, design_compression_strength: 21)
+        )
+      end
+
+      describe 'when computed ratio can be supplied' do
+        let(:analysis_results) { { computed_ratio: 0.0050, is_minimum_ratio: false } }
+
+        it 'is a success' do
+          expect(result_with_mocked_required_reinforcement_ratio).to be_a_success
+        end
+
+        it 'modifies footing by adding reinforcement' do
+          expect { result_with_mocked_required_reinforcement_ratio }.to change {
+            footing.reinforcement(direction: :length_1, above_middle: false).nil?
+          }.from(true).to(false)
+        end
+
+        it 'adds reinforcement with the correct ratio' do
+          result_with_mocked_required_reinforcement_ratio
+
+          expect(
+            footing.reinforcement_ratio(direction: :length_1, above_middle: false) >= analysis_results[:computed_ratio]
+          ).to eq(true)
+        end
+
+        it 'adds reinforcement layer coordinates for start' do
+          result_with_mocked_required_reinforcement_ratio
+
+          expect(
+            footing.coordinates_system.find_location('reinforcement_layer_start_location_length_1_bottom')
+          ).not_to be_nil
+        end
+
+        it 'adds reinforcement layer coordinates for end' do
+          result_with_mocked_required_reinforcement_ratio
+
+          expect(
+            footing.coordinates_system.find_location('reinforcement_layer_end_location_length_1_bottom')
+          ).not_to be_nil
+        end
+      end
+
+      describe "when computed ratio can't be supplied" do
+        let(:analysis_results) { { computed_ratio: 1.000, is_minimum_ratio: false } }
+
+        it 'is a failure' do
+          expect(result_with_mocked_required_reinforcement_ratio).to be_a_failure
+        end
+
+        it "doesn't modify footing by adding reinforcement" do
+          expect { result_with_mocked_required_reinforcement_ratio }.not_to(
+            change { footing.reinforcement(direction: :length_1, above_middle: false).nil? }
+          )
+        end
+
+        it 'adds reinforcement layer coordinates for start' do
+          result_with_mocked_required_reinforcement_ratio
+
+          expect(
+            footing.coordinates_system.find_location('reinforcement_layer_start_location_length_1_bottom')
+          ).not_to be_nil
+        end
+
+        it 'adds reinforcement layer coordinates for end' do
+          result_with_mocked_required_reinforcement_ratio
+
+          expect(
+            footing.coordinates_system.find_location('reinforcement_layer_end_location_length_1_bottom')
+          ).not_to be_nil
+        end
+      end
+    end
+  end
+end
